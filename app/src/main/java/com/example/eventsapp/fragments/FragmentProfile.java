f96f585db257f81a08c40bfa954b64fe67a2391d
Ben Zech <72528903+TechZech@users.noreply.github.com>
2021-10-10 19:40:30 -0400

    Profile Fragment loads username and image

--- app/src/main/java/com/example/eventsapp/fragments/FragmentProfile.java
+++ app/src/main/java/com/example/eventsapp/fragments/FragmentProfile.java
@@ -9,12 +9,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.eventsapp.R;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+
+import java.io.IOException;
+import java.security.Principal;
 
 
 public class FragmentProfile extends Fragment {
 
+    ParseUser currentUser = ParseUser.getCurrentUser();
+    private ImageView ivProfilePic;
+    private TextView tvUsername;
+    private Button btnSettings;
 
     public FragmentProfile() {
         // Required empty public constructor
@@ -23,8 +36,27 @@ public class FragmentProfile extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ivProfilePic = view.findViewById(R.id.ivProfilePic);
+        tvUsername = view.findViewById(R.id.tvProfileUsername);
+        btnSettings = view.findViewById(R.id.btnSettings);
 
+        // load user image from parse
+        ParseFile profilePicImage = currentUser.getParseFile("ProfilePicture");
+        if (profilePicImage != null) {
+            Glide.with(getContext()).load(profilePicImage.getUrl()).circleCrop().into(ivProfilePic);
+        } else {
+            Glide.with(getContext()).load(R.drawable.img_1).circleCrop().into(ivProfilePic);
+        }
 
+        //load username from parse
+        tvUsername.setText(currentUser.getUsername());
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go to a settings fragment?
+            }
+        });
     }
 
     @Override
